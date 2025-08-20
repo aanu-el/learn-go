d95364193910f14853564dde70059fe9faa0bf73
Eleos <peaceaanu@gmail.com>
2025-08-20 11:17:37 +0100

    maps & structs

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
-);
+)
+
+var orders = map[string]float64{
+	"burger": 5.99,
+	"fries": 2.49,
+	"drink": 1.99,
+}
+
+type Bill struct {
+	name string
+	items map[string]float64
+	tip float64
+}
 
 func main() {
-	fmt.Printf("Welcome to the Go programming language!\n")
+	for k,v := range orders {
+		fmt.Println("Item:", k, "Price:", v)
+	}
 }
